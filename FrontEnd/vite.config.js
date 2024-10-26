f35d268f71955142d9b696d98e83cd8231e8807c
Syed Ali Murtaza Bokhari <syedalimurtaza36@gmail.com>
2024-10-26 14:17:01 +0500

    update vercel and vite config

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,33 +1,36 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-  import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react';
-  import path from 'path';
-
-  export default defineConfig({
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@components': path.resolve(__dirname, 'src/Components'),
-        '@context': path.resolve(__dirname, 'src/Context'),
-        '@hooks': path.resolve(__dirname, 'src/Hooks'),
-        '@pages': path.resolve(__dirname, 'src/Pages'),
-        '@utils': path.resolve(__dirname, 'src/Utils'),
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/Components'),
+      '@context': path.resolve(__dirname, 'src/Context'),
+      '@hooks': path.resolve(__dirname, 'src/Hooks'),
+      '@pages': path.resolve(__dirname, 'src/Pages'),
+      '@utils': path.resolve(__dirname, 'src/Utils'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://crmsuite.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-    server: {
-      proxy: {
-        '/api': 'https://crmsuite.onrender.com' // Proxy '/api' to backend
-      },
-    },
-    build: {
-      rollupOptions: {
-          output:{
-              manualChunks(id) {
-                  if (id.includes('node_modules')) {
-                      return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                  }
-              }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
-      }
-  }
-  });
+        },
+      },
+    },
+  },
+});
